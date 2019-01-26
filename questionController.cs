20dcc58d18dacfe0018a14a828490d9777dd9947
mohameahmedomer <zackdad88@gmail.com>
2019-01-26 14:25:34 +0300

    Add files via upload

---
+++ questionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+using TaskTrial2.Models;
+namespace TaskTrial2.Controllers
+{
+    public class questionController : Controller
+    {
+        // GET: question
+        public ActionResult Index()
+        {
+
+
+            RootObject questions = null;
+
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            using (var Client = new HttpClient(handler))
+            {
+                Client.BaseAddress = new Uri("https://api.stackexchange.com/");
+                //HTTP GET
+                Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jason"));
+                var response = Client.GetAsync("2.2/questions?page=1&pagesize=50&order=desc&sort=creation&site=stackoverflow");
+                response.Wait();
+   
+                    var readTask = (response.Result).Content.ReadAsAsync<RootObject>();
+                    readTask.Wait();
+                    questions = readTask.Result;
+                ViewBag.result = questions;
+
+                
+
+            }
+
+            return View(questions);
+        }
+    }
+}
